89782aae99f35ca39c7fa183d20b83c8ef74f9bb
Thiago Barroso <thiagoo.silva@live.com>
2024-08-13 23:15:10 -0300

    página de login

--- src/componentes/login/Login.jsx
+++ src/componentes/login/Login.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" elemen={<LoginForm />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default Login;
